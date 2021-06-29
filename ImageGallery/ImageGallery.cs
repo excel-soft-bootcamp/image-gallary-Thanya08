eb707434da5ca875b1ae8dfcbaea74ea58248ca8
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-29 14:43:19 +0530

    Add files via upload

---
+++ ImageGallery/ImageGallery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageGallery
+{
+    class ImageGallery
+    {
+        Iphotos _photos;
+        public ImageGallery(Iphotos _photos)
+        {
+            this._photos = _photos;
+        }
+        public void Share()
+        {
+            _photos.Send();
+
+        }
+    }
+}
---
+++ ImageGallery/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageGallery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gmail _mailAccount = new Gmail();
+            ImageGallery mail = new ImageGallery(_mailAccount);
+            mail.Share();
+
+            Bluetooth _bluetoothDevice = new Bluetooth();
+            ImageGallery bluetooth = new ImageGallery(_bluetoothDevice);
+            bluetooth.Share();
+
+            Whatsapp messenger = new Whatsapp();
+            ImageGallery _messenger = new ImageGallery(messenger);
+            _messenger.Share();
+
+        }
+    }
+
+ }
+    
+
